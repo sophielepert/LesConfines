0e8af112a2e0fc9590053acc591e7f11a72bbc9a
Ambre-Thevenin <ambre.thvn@gmail.com>
2021-09-30 16:47:59 +0200

    Added text to all choices and cards styling

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 // JS pour survival.html
 
-const solo = document.querySelector('button');
-
+const solo = document.getElementById('solo');
 solo.addEventListener('click', function(){
     const soloStory = document.getElementById('solo-story');
+    const duoStory = document.getElementById('duo-story');
+    const tribeStory = document.getElementById('tribe-story');
     if (soloStory.style.display === "none") {
         soloStory.style.display = "block";
+        duoStory.style.display ="none";
+        tribeStory.style.display ="none";
       } else {
         soloStory.style.display = "none";
+      }
+})
+const duo = document.getElementById('duo');
+duo.addEventListener('click', function(){
+    const soloStory = document.getElementById('solo-story');
+    const duoStory = document.getElementById('duo-story');
+    const tribeStory = document.getElementById('tribe-story');
+    if (duoStory.style.display === "none") {
+        duoStory.style.display = "block";
+        tribeStory.style.display ="none";
+        soloStory.style.display = "none";
+      } else {
+        duoStory.style.display = "none";
+      }
+})
+
+const tribe = document.getElementById('tribe');
+tribe.addEventListener('click', function(){
+    const soloStory = document.getElementById('solo-story');
+    const duoStory = document.getElementById('duo-story');
+    const tribeStory = document.getElementById('tribe-story');
+    if (tribeStory.style.display === "none") {
+        tribeStory.style.display = "block";
+        duoStory.style.display = "none";
+        soloStory.style.display = "none";
+      } else {
+        tribeStory.style.display = "none";
       }
 })
