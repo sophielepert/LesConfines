35dfe2a438b1e9cf344440243e7c767373912c82
sophielepert <sophie.lepert@gmail.com>
2021-10-05 11:55:06 +0200

    add changes on script file with the random link footer

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function menuFerme(){
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*9)
+    var links=new Array()
+    links[0]="https://www.instagram.com/la.minute.culture/?hl=fr"
+    links[1]="https://www.lesothers.com/"
+    links[2]=="https://www.votretourdumonde.com/"
+    links[3]="https://www.travel-me-happy.com/"
+    links[4]="https://www.madame-oreille.com/"
+    links[5]="https://www.window-swap.com/Window"
+    links[6]="https://artsandculture.google.com/partner/van-gogh-museum"
+    links[7]="https://artsandculture.google.com/partner/the-metropolitan-museum-of-art"
+    links[8]="https://artsandculture.google.com/partner/museo-reina-sofia"
+    links[9]="http://www.worldwidetelescope.org/webclient/"
+
+    window.location=links[myrandom]
+}
