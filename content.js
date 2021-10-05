1e3af04c586663946188420678c65b87aa706dd2
110111-1 <moty.philippe@free.fr>
2021-10-06 01:04:11 +0200

    bug on responsive

---
+++ content.js
@@ -0,0 +1,9 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-let acc = document.getElementsByClassName("accordion");
-let i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
-}
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
