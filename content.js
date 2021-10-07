9c7144f0b81b27e21cb5d041754ac363df9731d4
sophielepert <sophie.lepert@gmail.com>
2021-10-07 16:13:15 +0200

    add some changes in content files and add menu on it

--- content.js
+++ content.js
@@ -7,3 +7,23 @@ for (i = 0; i < acc.length; i++) {
         this.nextElementSibling.classList.toggle("show");
     }
 }
+
+// -------------------------------START JS Header --------------------------
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", menuDeroulant);
+function menuDeroulant() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+navLink.forEach(n => n.addEventListener("click", menuFerme));
+function menuFerme(){
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// -------------------------------End JS Header --------------------------
